08c0cec0265428683b41fb0c983093a3d6b292e3
RocioSNg <rocio.s.ng@gmail.com>
2016-07-27 20:16:46 -0700

    typo fixing

--- data_combiner.R
+++ data_combiner.R
@@ -1,7 +1,7 @@
 # Script to read in all of the google analytics queries
 
 # Deal with the weird format and headers
-queary_reader <- function(data_file){
+query_reader <- function(data_file){
     read.delim(data_file ,sep="\t", header=TRUE,
               fileEncoding="UTF-16", skip = 15)
     }
@@ -12,7 +12,7 @@ file_list <- list.files("data")[grep("query", list.files("data"))]
 # Loop though reading all files and combining them
 for (x in file_list){
     file_path <- paste0("data/", x)
-    query_data <- queary_reader(file_path)
+    query_data <- query_reader(file_path)
     if (exists("all_data")) {
         all_data <- rbind(all_data, query_data)
     }else{
