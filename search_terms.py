5b86904f61060b8c69914126773326d51d98fe6e
baolinliu <baolin.liu@gmail.com>
2016-08-15 16:02:47 -0700

    Add files via upload

--- search_terms.py
+++ search_terms.py
@@ -7,8 +7,6 @@ and the actual relevant keyword search used.
 import pandas as pd
 import re
 
-street = pd.read_csv('Street_Names.csv')
-
 street_names = pd.read_csv('street_names_clean.csv')
 
 street_name_list = list(street_names['Names'].values)
@@ -26,13 +24,14 @@ def search_term_type(text):
     
     """
     input: keyword search term
-    output: labels the keyword as good, address, link.
+    output: labels the keyword as good, address, year, link.
     
     """
+    #street_type = '\s(aly|ave*|blvd|boulevard|cir*|court|ct|dr*|exp*|hl|hwy|highway|ln|lane|park|path|pl*|ramp|rd|road|row|st*|step*|stps|walk|way)'
     
     text = removePunctuation(text)
-    address = [re.findall('\d\s'+street,text) for street in street_name_list 
-               if re.findall('\d\s'+ street, text)!= []]
+    address = [re.findall('\d+\s'+street+'|'+street+'\s\w+',text) for street in street_name_list 
+               if re.findall('\d+\s'+street+'|'+street+'\s\w+', text)!= []]
     
     links_regex = "http[s]*|www.*"
 
@@ -45,7 +44,7 @@ def search_term_type(text):
         term =  "Link"
     else:
         term = "Search Term"
-    return term
+    return term , address
 
 # http://stackoverflow.com/questions/6718633/python-regular-expression-again-match-url
 # http://regexlib.com/REDetails.aspx?regexp_id=430
