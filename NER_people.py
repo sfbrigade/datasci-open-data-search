00c1f3e732efc207014516f3cffbfa4719f97fe4
baolinliu <baolin.liu@gmail.com>
2016-09-28 19:45:08 -0700

    Add files via upload

--- NER_people.py
+++ NER_people.py
@@ -1,9 +1,10 @@
 import cPickle, numpy as np, pandas as pd , re
+from collections import Counter
 from polyglot.text import Text
 
 tagged_search = pd.read_csv('processed_search_term_data/tagged_search_terms.csv')
 
-tagged_search_list = list(set(tagged_search['processed_search_term']))
+tagged_search_list = list(tagged_search['processed_search_term'])
 
 def removePunctuation(text):
 
@@ -16,28 +17,38 @@ string_word = removePunctuation(str(tagged_search_list))
 
 def people(string):
     '''
-
-    ****This function will work after much better if you have cleaned and identified relevant search terms first.
-    Removing addresses first is key since names of streets are name of people and we dont want that.****
+    This function will work after much better if you have cleaned 
+    and identified relevant search terms.
     
     Input: A string of relevant search terms
-    Output: pulls out names of people identified from the Named Entity Recognition software polyglot
-
-    The function actives the Text class and then uses the entities function to identify person organization and 
-    location.  It isolates only for persons, gets rid of I-PER statement and the 'u' statement in the beginning
-    of unicode strings and joins the string back together, it does a set of each list and then the overall list
-    to rid of duplicate findngs.  
-
+    Output: pulls out names of people identified from the Named Entity Recognition 
+    software polyglot
     '''
     NER = Text(string)
     NER = NER.entities
     ent = [removePunctuation(re.sub('I-PER','',str(entity))) for entity in NER if entity.tag == "I-PER"]
     ent =[' '.join(set([w[1:] for w in word.split(' ')])) for word in ent]
-    return list(set(ent))
+    return list((ent))
 
 string = people(string_word) 
 
-cPickle.dump(string,open('NER_people_list.p','wb'))
+S = Counter(string).most_common()
+
+name = []
+count = []
+
+for name_count in (S):
+    name.append(name_count[0])
+    count.append(name_count[1])
+
+table = pd.DataFrame(columns=['Name','Count'])
+
+table['Name'] = name
+table['Count'] = count
+
+table.to_csv('people_count.csv')
+
+#cPickle.dump(string,open('NER_people_list.p','wb'))
 # creates a list of saved file
 # to load:
 
